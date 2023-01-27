dcbf5025ad310b0c93abcab1e1af3fc034de25f1
aramhovhannisyan02 <115581991+aramhovhannisyan02@users.noreply.github.com>
2023-01-27 12:07:02 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,152 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', 416)
+Config.set('graphics', 'height', 720)
+Config.set('graphics', 'resizable', 0)
+
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
+from kivy.graphics import Color
+
+# import sqlite3 as sql3
+
+
+# con = sql3.connect('users.db')
+# with open('users.db', 'a') as con:
+#     con = con
+
+# with con:
+#     con.execute("""
+#         CREATE TABLE users (
+#             Name,
+#             Email,
+#             Password
+#         );
+#     """)
+
+
+class CreateAccountWindow(Screen):
+    namee = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def submit(self):
+        if self.namee.text != "" and self.email.text != "" and self.email.text.count("@") == 1 and self.email.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.email.text, self.password.text, self.namee.text)
+               
+                # # подготавливаем множественный запрос
+                # sql = 'INSERT INTO users (Name, Email, Password) values(?, ?, ?)'
+                # # указываем данные для запроса
+                # data = [
+                #     (self.email.text, self.password.text, self.namee.text),
+                # ]
+                # # добавляем с помощью множественного запроса все данные сразу
+                # with con:
+                #     con.executemany(sql, data)
+
+                # # выводим содержимое таблицы на экран
+                # with con:
+                #     data = con.execute("SELECT * FROM users")
+                #     for row in data:
+                #         print(row)
+               
+                self.reset()
+
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+        self.namee.text = ""
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "log in"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
+
+
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
+
+
+class MyMainApp(App):
+    def build(self):
+        return sm
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
